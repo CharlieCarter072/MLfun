cb00de5c10ebf3b4df09d36e290e56c34c5a1e4b
CharlieCarter072 <charliecarter072@gmail.com>
2024-05-14 17:02:40 -0600

    display images, notes for future fixes

--- LFS/Load_Data.py
+++ LFS/Load_Data.py
@@ -3,7 +3,7 @@ from Programs.mathFuncts import *
 
 def loadTrainingDataMatrix():  # row 0 is labels,
     with open("LFS/train.csv", "r") as train:
-        full_unformatted_data = train.readlines()
+        full_unformatted_data = train.readlines()[:100:]  # the [:100:] is for fast loading purposes
         temp_matrix_full = Matrix([list(map(fix_brightness_values, (i.strip().split(",")))) for i in full_unformatted_data])  # messes up labels
         train_matrix_full = Matrix([temp_matrix_full.column(i) for i in range(785)])
 
@@ -12,8 +12,15 @@ def loadTrainingDataMatrix():  # row 0 is labels,
 
 def loadTestingDataMatrix():
     with open("LFS/test.csv", "r") as train:
-        full_unformatted_data = train.readlines()
+        full_unformatted_data = train.readlines()[:100:]  # the [:100:] is for fast loading purposes
 
         temp_matrix_full = Matrix([list(map(fix_brightness_values, (i.strip().split(",")))) for i in full_unformatted_data])
         train_matrix_full = Matrix([temp_matrix_full.column(i) for i in range(785)])
         return train_matrix_full
+
+
+def display_digit(data_in):
+    for i in range(28):
+        print([{0: " ", 1: "#"}[round(data_in[28*i + j][0])] for j in range(28)])
+
+
--- main.py
+++ main.py
@@ -1,18 +1,20 @@
-# input vector, multiply with weights -> activation for next layer (don't forget bias)
-# input (), 1st layer (16), 2nd layer (16), output (10)
-# due to changes, this whole file will probably have to be redone
-
 from LFS.Load_Data import *
 from Programs.network import *
 
-print("\nStarted program, loading data & weights...\n")  # initialize program
 
-data_matrix = loadTrainingDataMatrix()  # should be testing data, but for home testing the testing data doesn't have a label, so it screws up
-data_matrix_no_labels = Matrix(data_matrix[1::])
+def main():
+    print("\nStarted program, loading data & weights...\n")  # initialize program
 
-print("Loading complete\n")
+    data_matrix = loadTrainingDataMatrix()  # should be testing data, but for home testing the testing data doesn't have a label, so it screws up
+    data_matrix_no_labels = Matrix(data_matrix[1::])
 
-Testing_network = Network(16,16)
-test_input = Matrix([[i] for i in data_matrix_no_labels.column(0)])
-print(Testing_network.raw_prediction(test_input))
-print(f"expected result: {data_matrix.items[0][0] * 255}")  # the * 255 is sketchy, but alright for now
+    print("Loading complete\n")
+
+    Testing_network = Network(16,16)
+    test_input = Matrix([[i] for i in data_matrix_no_labels.column(0)])  # fix this to be in the right plac3e
+    #print(display_digit(test_input))
+    print(Testing_network.raw_prediction(test_input))
+    print(f"expected result: {data_matrix.items[0][0] * 255}")  # the * 255 is sketchy, but alright for now, also sketchy labels
+
+main()
+
